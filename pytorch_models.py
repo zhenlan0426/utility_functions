aaf77122bdc9f39138a9ffd8ad6ae032da752b92
will <zhenlanwang0426@gmail.com>
2019-04-15 10:45:18 -0400

    Update pytorch_models.py

--- pytorch_models.py
+++ pytorch_models.py
@@ -277,4 +277,125 @@ class SincConv_fast(nn.Module):
                          bias=None, groups=1) 
 
 
-    
+class SincConv_fast2(nn.Module):
+    """Sinc-based convolution
+    Parameters
+    ----------
+    in_channels : `int`
+        Number of input channels. Must be 1.
+    out_channels : `int`
+        Number of filters.
+    kernel_size : `int`
+        Filter length.
+    sample_rate : `int`, optional
+        Sample rate. Defaults to 16000.
+    Usage
+    -----
+    See `torch.nn.Conv1d`
+    Reference
+    ---------
+    Mirco Ravanelli, Yoshua Bengio,
+    "Speaker Recognition from raw waveform with SincNet".
+    https://arxiv.org/abs/1808.00158
+    """
+
+    @staticmethod
+    def to_mel(hz):
+        return 2595 * np.log10(1 + hz / 700)
+
+    @staticmethod
+    def to_hz(mel):
+        return 700 * (10 ** (mel / 2595) - 1)
+
+    def __init__(self, in_channels, out_channels, kernel_size, sample_rate=16000, 
+                 stride=1, padding=0, dilation=1, bias=False, groups=1, min_low_hz=50, min_band_hz=50):
+
+        super(SincConv_fast,self).__init__()
+
+        self.out_channels = out_channels
+        self.in_channels = in_channels
+        self.kernel_size = kernel_size
+        
+        # Forcing the filters to be odd (i.e, perfectly symmetrics)
+        if kernel_size%2==0:
+            self.kernel_size=self.kernel_size+1
+            
+        self.stride = stride
+        self.padding = padding
+        self.dilation = dilation
+
+        if bias:
+            raise ValueError('SincConv does not support bias.')
+        if groups > 1:
+            raise ValueError('SincConv does not support groups.')
+
+        self.sample_rate = sample_rate
+        self.min_low_hz = min_low_hz
+        self.min_band_hz = min_band_hz
+
+        # initialize filterbanks such that they are equally spaced in Mel scale
+        low_hz = 10
+        high_hz = self.sample_rate / 2 - (self.min_low_hz + self.min_band_hz)
+        diff = high_hz - low_hz
+        interval = diff//out_channels
+        
+        # filter lower frequency (out_channels, 1)
+        self.low_hz_ = nn.Parameter(torch.rand((out_channels,in_channels,1))*diff+low_hz)
+
+        # filter frequency band (out_channels, 1)
+        self.band_hz_ = nn.Parameter(torch.rand((out_channels,in_channels,1))*interval)
+
+        # Hamming window
+        #self.window_ = torch.hamming_window(self.kernel_size)
+        n_lin=torch.linspace(0, (self.kernel_size/2)-1, steps=int((self.kernel_size/2))) # computing only half of the window
+        self.window_=0.54-0.46*torch.cos(2*math.pi*n_lin/self.kernel_size);
+
+
+        # (kernel_size, 1)
+        n = (self.kernel_size - 1) / 2.0
+        self.n_ = 2*math.pi*torch.arange(-n, 0) / self.sample_rate # Due to symmetry, I only need half of the time axes
+
+ 
+
+
+    def forward(self, waveforms):
+        """
+        Parameters
+        ----------
+        waveforms : `torch.Tensor` (batch_size, 1, n_samples)
+            Batch of waveforms.
+        Returns
+        -------
+        features : `torch.Tensor` (batch_size, out_channels, n_samples_out)
+            Batch of sinc filters activations.
+        """
+
+        self.n_ = self.n_.to(waveforms.device)
+
+        self.window_ = self.window_.to(waveforms.device)
+
+        low = self.min_low_hz  + torch.abs(self.low_hz_)
+        
+        high = torch.clamp(low + self.min_band_hz + torch.abs(self.band_hz_),self.min_low_hz,self.sample_rate/2)
+        band=(high-low)
+        
+        f_times_t_low = torch.mul(low, self.n_)
+        f_times_t_high = torch.mul(high, self.n_)
+
+        band_pass_left=((torch.sin(f_times_t_high)-torch.sin(f_times_t_low))/(self.n_/2))*self.window_ # Equivalent of Eq.4 of the reference paper (SPEAKER RECOGNITION FROM RAW WAVEFORM WITH SINCNET). I just have expanded the sinc and simplified the terms. This way I avoid several useless computations. 
+        band_pass_center = 2*band
+        band_pass_right= torch.flip(band_pass_left,dims=[2])
+        
+        
+        band_pass=torch.cat([band_pass_left,band_pass_center,band_pass_right],dim=2)
+
+        
+        band_pass = band_pass / (band_pass_center)
+        
+
+        self.filters = (band_pass).view(
+            self.out_channels, self.in_channels, self.kernel_size)
+
+        return F.conv1d(waveforms, self.filters, stride=self.stride,
+                        padding=self.padding, dilation=self.dilation,
+                         bias=None, groups=1)    
