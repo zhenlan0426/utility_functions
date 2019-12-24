7f497e2c1383720f5a77098fb4cbe81b6978f17f
will <zhenlanwang0426@gmail.com>
2019-12-23 20:14:14 -0500

    Add files via upload

--- pytorch_util.py
+++ pytorch_util.py
@@ -1,11 +1,13 @@
 import torch.nn as nn
-from pretrainedmodels import utils
+#from pretrainedmodels import utils
 import torch
 import time
 import numpy as np
 from torch.nn.utils import clip_grad_value_
 import copy
 from torch.utils.data import Dataset
+from torch.optim.optimizer import Optimizer
+import math
 
 ''' functions related to fine-tuning pretrained model '''
 def gather_parameter_byName(name,model):
@@ -40,13 +42,13 @@ def fine_tune_pretrainedmodels(model, outDim):
     model.last_linear = nn.Linear(InDim, outDim)
     return model
 
-def feature_extract_pretrainedmodels(model):
-    # works only for models in pretrainedmodels as it depends on its API
-    # replace last linear with identity function
-    # set grad to False
-    model.last_linear = utils.Identity()
-    set_requires_grad(model,False)
-    return model
+# def feature_extract_pretrainedmodels(model):
+#     # works only for models in pretrainedmodels as it depends on its API
+#     # replace last linear with identity function
+#     # set grad to False
+#     model.last_linear = utils.Identity()
+#     set_requires_grad(model,False)
+#     return model
 
 def differential_lr(model,base_lr,factor=2.6):
     # assume model is instance of Sequential
@@ -243,3 +245,76 @@ def wrapTrainGen2TestGen(trainGen,arg=0):
         yield data[arg]
 
 
+class RAdam(Optimizer):
+    def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-8, weight_decay=0):
+        defaults = dict(lr=lr, betas=betas, eps=eps, weight_decay=weight_decay)
+        self.buffer = [[None, None, None] for ind in range(10)]
+        super(RAdam, self).__init__(params, defaults)
+
+    def __setstate__(self, state):
+        super(RAdam, self).__setstate__(state)
+
+    def step(self, closure=None):
+
+        loss = None
+        if closure is not None:
+            loss = closure()
+
+        for group in self.param_groups:
+
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+                grad = p.grad.data.float()
+                if grad.is_sparse:
+                    raise RuntimeError('RAdam does not support sparse gradients')
+
+                p_data_fp32 = p.data.float()
+
+                state = self.state[p]
+
+                if len(state) == 0:
+                    state['step'] = 0
+                    state['exp_avg'] = torch.zeros_like(p_data_fp32)
+                    state['exp_avg_sq'] = torch.zeros_like(p_data_fp32)
+                else:
+                    state['exp_avg'] = state['exp_avg'].type_as(p_data_fp32)
+                    state['exp_avg_sq'] = state['exp_avg_sq'].type_as(p_data_fp32)
+
+                exp_avg, exp_avg_sq = state['exp_avg'], state['exp_avg_sq']
+                beta1, beta2 = group['betas']
+
+                exp_avg_sq.mul_(beta2).addcmul_(1 - beta2, grad, grad)
+                exp_avg.mul_(beta1).add_(1 - beta1, grad)
+
+                state['step'] += 1
+                buffered = self.buffer[int(state['step'] % 10)]
+                if state['step'] == buffered[0]:
+                    N_sma, step_size = buffered[1], buffered[2]
+                else:
+                    buffered[0] = state['step']
+                    beta2_t = beta2 ** state['step']
+                    N_sma_max = 2 / (1 - beta2) - 1
+                    N_sma = N_sma_max - 2 * state['step'] * beta2_t / (1 - beta2_t)
+                    buffered[1] = N_sma
+
+                    # more conservative since it's an approximated value
+                    if N_sma >= 5:
+                        step_size = group['lr'] * math.sqrt((1 - beta2_t) * (N_sma - 4) / (N_sma_max - 4) * (N_sma - 2) / N_sma * N_sma_max / (N_sma_max - 2)) / (1 - beta1 ** state['step'])
+                    else:
+                        step_size = group['lr'] / (1 - beta1 ** state['step'])
+                    buffered[2] = step_size
+
+                if group['weight_decay'] != 0:
+                    p_data_fp32.add_(-group['weight_decay'] * group['lr'], p_data_fp32)
+
+                # more conservative since it's an approximated value
+                if N_sma >= 5:                    
+                    denom = exp_avg_sq.sqrt().add_(group['eps'])
+                    p_data_fp32.addcdiv_(-step_size, exp_avg, denom)
+                else:
+                    p_data_fp32.add_(-step_size, exp_avg)
+
+                p.data.copy_(p_data_fp32)
+
+        return loss
